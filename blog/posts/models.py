8f67195c8ba488f3237b05b39ef11b9eb678df77
cheikhou <dramecheikhou@gmail.com>
2022-06-16 18:06:34 +0000

    first commit

---
+++ blog/posts/models.py
@@ -0,0 +1,40 @@
+"get_user_model renvoie le modèle d'utilisateur actuellement actif"
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.urls import reverse
+
+User = get_user_model()
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255, unique=True, verbose_name="Titre")
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    created_on = models.DateField(blank=True, null=True)
+    published = models.BooleanField(default=True, verbose_name="publié")
+    contend = models.TextField(blank=True,verbose_name="contenu")
+    thumbnail = models.ImageField(blank=True, upload_to='blog')
+
+    class Meta():
+        ordering = ['-created_on']
+        verbose_name = 'article'
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args,**kwargs)
+#  test
+    @property
+    def author_or_default(self):
+        return self.author.username if self.author else "Auteur inconnu"
+
+
+    def get_absolute_url(self):
+        return reverse('posts:home')
+
+
